abb2a0a02a6a8be612c6b8a81df395ef65cd9926
klingj3 <Gobosox09!github>
2018-04-20 21:30:17 -0400

    Prevents skillbar animation until scrolled to.

--- js/styling.js
+++ js/styling.js
@@ -1,7 +1,11 @@
-jQuery(document).ready(function(){
+$(window).on('scroll', function() {
     jQuery('.skillbar').each(function(){
-        jQuery(this).find('.skillbar-bar').animate({
-            width:jQuery(this).attr('data-percent')
-        },6000);
+        var pos = $(this).offset().top;
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + $(window).height() - 200){
+            jQuery(this).find('.skillbar-bar').animate({
+                width:jQuery(this).attr('data-percent')
+            },6000);
+        }
     });
 });
