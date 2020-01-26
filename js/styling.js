0df6f8485f8a75a5369713227b78b528992aef53
John Klingelhofer <klingj3@rpi.edu>
2020-01-26 18:15:46 -0500

    Throw a simple animation on the main landing page's title, clean up some old and unnecessary elements.

--- js/styling.js
+++ js/styling.js
@@ -19,7 +19,7 @@ function getTransformValue(v1, v2, value) {
   return val.toFixed(1);
 }
 
-function cardAnimations() {
+function initialAnimation() {
   // Transform the card on mousemove
   document.addEventListener('mousemove', function (event) {
       let card_x = getTransformValue(event.clientX, window.innerWidth, 56, true);
@@ -28,8 +28,23 @@ function cardAnimations() {
   });
 }
 
+function loadAnimation() {
+    d3.select('.name')
+        .style("transform", "translate(0px, -30px)")
+        .transition()
+        .delay(500)
+        .duration(1000)
+        .style("transform", "translate(0px, 0px")
+        .style("opacity", "1");
+    d3.selectAll('.self-statement-item')
+        .transition()
+        .delay(function(d, i){return (1700 + 1500*i)})
+        .duration(500)
+        .style('opacity', '1');
+}
+
 window.onload = function () {
-  cardAnimations();
-  animateSelfStatement();
+  loadAnimation();
+  initialAnimation();
 };
 
